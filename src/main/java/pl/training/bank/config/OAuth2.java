aa35bdcfb11aca3ad381b2ad56fca3f822461351
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-05-26 13:00:10 +0200

    sDAD

--- src/main/java/pl/training/bank/config/OAuth2.java
+++ src/main/java/pl/training/bank/config/OAuth2.java
@@ -1,11 +1,9 @@
 package pl.training.bank.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -22,7 +20,7 @@ public class OAuth2 {
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/api/**").hasRole("ADMIN");
+            http.authorizeRequests().antMatchers("/api-v1/**").hasRole("ADMIN");
         }
 
     }
@@ -33,20 +31,18 @@ public class OAuth2 {
 
         @Autowired
         private AuthenticationManager authenticationManagerBean;
-        @Autowired
-        private UserDetailsService userDetailsService;
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients.inMemory()
                     .withClient("rest")
-                    .authorizedGrantTypes("password", "refresh_token")
+                    .authorizedGrantTypes("password")
                     .scopes("read", "write");
         }
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints.authenticationManager(authenticationManagerBean).userDetailsService(userDetailsService);
+            endpoints.authenticationManager(authenticationManagerBean);
         }
 
         @Override
