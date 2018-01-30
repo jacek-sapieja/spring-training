93810e094310b644a261ebb5b1b95766306bc3c5
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-01-30 16:23:26 +0100

    Update

--- src/main/java/pl/training/bank/config/Mvc.java
+++ src/main/java/pl/training/bank/config/Mvc.java
@@ -4,16 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Import(Beans.class)
 @EnableWebMvc
 @Configuration
-public class Mvc extends WebMvcConfigurerAdapter {
+public class Mvc implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver viewResolver() {
@@ -33,4 +30,5 @@ public class Mvc extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("index.html").setViewName("index");
     }
+
 }
